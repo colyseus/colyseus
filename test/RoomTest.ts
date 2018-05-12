6326e727901d1daacf63cd3db057ae4db334984b
Endel Dreyer <endel.dreyer@gmail.com>
2018-05-12 15:42:24 -0300

    fix testing for cleared interval on nodev10

--- test/RoomTest.ts
+++ test/RoomTest.ts
@@ -61,10 +61,10 @@ describe('Room', function() {
       var client = createDummyClient();
 
       (<any>room)._onJoin(client);
-      assert.ok((<any>room)._patchInterval._repeat > 0);
+      assert.ok((<any>room)._patchInterval._idleTimeout > 0);
 
       room.on('dispose', function() {;
-        assert.ok(!(<any>room)._patchInterval._repeat);
+        assert.ok((<any>room)._patchInterval._idleTimeout === -1);
         done();
       });
 
