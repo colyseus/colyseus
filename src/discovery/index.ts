6579d5ea7bd0ab7595569444ef1ec61816c9f122
Endel Dreyer <endel.dreyer@gmail.com>
2019-09-19 14:28:59 -0300

    use internal-ip instead of public-ip package. #264

--- src/discovery/index.ts
+++ src/discovery/index.ts
@@ -1,4 +1,4 @@
-import ip from 'public-ip';
+import ip from 'internal-ip';
 import { Presence } from '../presence/Presence';
 
 const NODES_SET = 'colyseus:nodes';
@@ -9,22 +9,19 @@ export interface Node {
     processId: string;
 }
 
-function getNodeAddress(node: Node, cb: (nodeAddress: string) => any) {
-    ip.v4().then((publicAddress: string) => {
-        return cb(node.processId + '/' + publicAddress + ':' + node.port);
-    });
+async function getNodeAddress(node: Node) {
+  const ipv4 = await ip.v4();
+  return `${node.processId}/${ipv4}:${node.port}`;
 }
 
-export function registerNode(presence: Presence, node: Node) {
-    getNodeAddress(node, (nodeAddress: string) => {
-        presence.sadd(NODES_SET, nodeAddress);
-        presence.publish(DISCOVERY_CHANNEL, `add,${nodeAddress}`);
-    });
+export async function registerNode(presence: Presence, node: Node) {
+  const nodeAddress = await getNodeAddress(node);
+  presence.sadd(NODES_SET, nodeAddress);
+  presence.publish(DISCOVERY_CHANNEL, `add,${nodeAddress}`);
 }
 
-export function unregisterNode(presence: Presence, node: Node) {
-    getNodeAddress(node, (nodeAddress: string) => {
-        presence.srem(NODES_SET, nodeAddress);
-        presence.publish(DISCOVERY_CHANNEL, `remove,${nodeAddress}`);
-    });
+export async function unregisterNode(presence: Presence, node: Node) {
+  const nodeAddress = await getNodeAddress(node);
+  presence.srem(NODES_SET, nodeAddress);
+  presence.publish(DISCOVERY_CHANNEL, `remove,${nodeAddress}`);
 }
