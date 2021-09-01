535cabbc4fbe6c7af14348a26735d8afec1da668
Lahiru Pathirage <lpathirage.c@respropmanagement.com>
2021-09-01 11:35:35 +0530

    #431 skip private packages

--- .github/scripts/package-publisher.cjs
+++ .github/scripts/package-publisher.cjs
@@ -9,12 +9,14 @@ function publishVersionChangedPackagesList(root_dir) {
     for (let index in packagesList) {
         try {
             const packageInfo = packagesList[index];
-            // Get published package info and latest version.
+            // Get published package latest version.
             const latestPublisedVersion = execSync(`npm show ${packageInfo['package']['name']} version`).toString().replace(/\n/g, "");
             console.log(`Package ${packageInfo['package']['name']} latest version in npm registry: ${latestPublisedVersion}`);
             console.log(`Package ${packageInfo['package']['name']} version in repository: ${packageInfo["package"]["version"]}`);
-            if (packageInfo["package"]["private"]) {
+            // Skip private packages
+            if (packageInfo["package"].hasOwnProperty("private") || packageInfo["package"]["private"]) {
                 console.log(`Skipping private package ${packageInfo["package"]["name"]}`);
+                continue;
             }
             // Check whether the directory packge is greater than the published.
             else if (semver.gt(packageInfo["package"]["version"], latestPublisedVersion)) {
@@ -23,7 +25,7 @@ function publishVersionChangedPackagesList(root_dir) {
                     { stdio: 'inherit' })
                 console.log(`publishing package ${packageInfo["package"]["name"]}`)
                 // Execute NPM commands
-                execSync(`cd ${package["location"]} && npm publish`, { stdio: 'inherit' });
+                execSync(`cd ${packageInfo["location"]} && npm publish`, { stdio: 'inherit' });
             }
         } catch (error) {
             continue;
