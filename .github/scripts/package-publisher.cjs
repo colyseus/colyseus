083f909d6b67244755c73138c6cf1de433a8002f
Endel Dreyer <endel.dreyer@gmail.com>
2021-09-01 14:52:18 -0300

    postmerge #438. skip private packages early to avoid requesting their version

--- .github/scripts/package-publisher.cjs
+++ .github/scripts/package-publisher.cjs
@@ -9,21 +9,25 @@ function publishVersionChangedPackagesList(root_dir) {
     for (let index in packagesList) {
         try {
             const packageInfo = packagesList[index];
-            // Get published package latest version.
-            const latestPublisedVersion = execSync(`npm show ${packageInfo['package']['name']} version`).toString().replace(/\n/g, "");
-            console.log(`Package ${packageInfo['package']['name']} latest version in npm registry: ${latestPublisedVersion}`);
-            console.log(`Package ${packageInfo['package']['name']} version in repository: ${packageInfo["package"]["version"]}`);
+
             // Skip private packages
             if (packageInfo["package"].hasOwnProperty("private") || packageInfo["package"]["private"]) {
                 console.log(`Skipping private package ${packageInfo["package"]["name"]}`);
                 continue;
             }
+
+            // Get published package latest version.
+            const latestPublisedVersion = execSync(`npm show ${packageInfo['package']['name']} version`).toString().replace(/\n/g, "");
+            console.log(`Package ${packageInfo['package']['name']} latest version in npm registry: ${latestPublisedVersion}`);
+            console.log(`Package ${packageInfo['package']['name']} version in repository: ${packageInfo["package"]["version"]}`);
+
             // Check whether the directory packge is greater than the published.
-            else if (semver.gt(packageInfo["package"]["version"], latestPublisedVersion)) {
+            if (semver.gt(packageInfo["package"]["version"], latestPublisedVersion)) {
                 // Set NPM tokens.
                 execSync(`npm config set //registry.npmjs.org/:_authToken ${process.env.NPM_TOKEN}`,
                     { stdio: 'inherit' })
                 console.log(`publishing package ${packageInfo["package"]["name"]}`)
+
                 // Execute NPM commands
                 execSync(`cd ${packageInfo["location"]} && npm publish`, { stdio: 'inherit' });
             }
