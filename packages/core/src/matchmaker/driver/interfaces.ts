a09717ac65df59f5af10856142d94f900bfa4f63
Lahiru Pathirage <lpsandaruwan@gmail.com>
2022-08-16 23:11:54 +0530

    Code Documentation Updates - Driver interface (#534)
    * CLYS-101 doc improvments
    
    * undelete package-lock
    
    * CLYS-101 code doc fixes
    
    * CLYS-120 driver inteface doc updates

--- packages/core/src/matchmaker/driver/interfaces.ts
+++ packages/core/src/matchmaker/driver/interfaces.ts
@@ -28,15 +28,44 @@ export interface QueryHelpers<T> {
 }
 
 export interface MatchMakerDriver {
+
+  /**
+   * Initialize a room cache which contains CRUD operations for room listings.
+   * 
+   * @param initialValues - Predefined room properties.
+   * 
+   * @returns RoomData - New room cache.
+   */
   createInstance(initialValues: any): RoomListingData;
 
+  /**
+   * Query rooms in room cache for given conditions.
+   * 
+   * @param conditions - Filtering conditions.
+   * 
+   * @returns Promise<RoomListingData[]> | RoomListingData[] - A promise or an object contaning room metadata list.
+   */
   find(
     conditions: Partial<IRoomListingData>,
     additionalProjectionFields?: any,
   ): Promise<RoomListingData[]> | RoomListingData[];
 
+  /**
+   * Query for a room in room cache for given conditions.
+   * 
+   * @param conditions - Filtering conditions.
+   * 
+   * @returns `RoomListingData` - An object contaning filtered room metadata.
+   */
   findOne(conditions: Partial<IRoomListingData>): QueryHelpers<RoomListingData>;
 
+  /**
+   * Empty the room cache.
+   */
   clear(): void;
+
+  /**
+   * Dispose the connection of the room cache medium.
+   */
   shutdown(): void;
 }
