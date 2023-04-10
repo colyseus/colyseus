7a5abc3a09b71ddbfd06c3790cfd5b435bc3b7cb
Wenish <Wenish@users.noreply.github.com>
2023-04-11 01:03:46 +0200

    Update index.ts

--- packages/core/src/discovery/index.ts
+++ packages/core/src/discovery/index.ts
@@ -12,8 +12,9 @@ export interface Node {
 async function getNodeAddress(node: Node) {
   const host = process.env.SELF_HOSTNAME || await ip.v4();
   const port = process.env.SELF_PORT ?? node.port;
+  const baseAddress = `${node.processId}/${host}`
   const shouldPortBeAddedToAddress = !!port;
-  return shouldPortBeAddedToAddress ? `${node.processId}/${host}:${port}` : `${node.processId}/${host}`;
+  return shouldPortBeAddedToAddress ? `${baseAddress}:${port}` : baseAddress;
 }
 
 export async function registerNode(presence: Presence, node: Node) {
