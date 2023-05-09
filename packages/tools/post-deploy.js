a64293e4e0cba0c7e21f62a9d4b18284a1fd45ea
Endel Dreyer <endel.dreyer@gmail.com>
2023-05-09 11:23:37 -0300

    update @colyseus/tools post-deploy script

--- packages/tools/post-deploy.js
+++ packages/tools/post-deploy.js
@@ -2,11 +2,13 @@
 const pm2 = require('pm2');
 const os = require('os');
 const fs = require('fs');
+const { exec } = require('child_process');
 
 const opts = { env: process.env.NODE_ENV || "production" };
 const maxCPU = os.cpus().length;
 
-const NGINX_CONFIG_FILE = '/etc/nginx/colyseus_servers.conf';
+const NGINX_SERVERS_CONFIG_FILE = '/etc/nginx/colyseus_servers.conf';
+const NGINX_LIMITS_CONFIG_FILE = '/etc/nginx/colyseus_limits.conf';
 
 pm2.list(function(err, apps) {
   bailOnErr(err);
@@ -34,24 +36,30 @@ pm2.list(function(err, apps) {
 });
 
 function updateAndReloadNginx() {
+
   pm2.list(function(err, apps) {
     bailOnErr(err);
 
-    const port = 2567;
-    const addresses = [];
-
-    apps.forEach(function(app) {
-      addresses.push(`127.0.0.1:${ port + app.pm2_env.NODE_APP_INSTANCE }`);
-    });
-
-    fs.writeFileSync(NGINX_CONFIG_FILE, addresses.map(address => `server ${address};`).join("\n"));
-
-    // "pm2 save"
-    pm2.dump(function(err, ret) {
+    // update file descriptor limits systemwide + nginx worker connections
+    updateNOFileConfig(function(err) {
       bailOnErr(err);
 
-      // exit with success!
-      process.exit();
+      const port = 2567;
+      const addresses = [];
+
+      apps.forEach(function(app) {
+        addresses.push(`127.0.0.1:${ port + app.pm2_env.NODE_APP_INSTANCE }`);
+      });
+
+      fs.writeFileSync(NGINX_SERVERS_CONFIG_FILE, addresses.map(address => `server ${address};`).join("\n"), bailOnErr);
+
+      // "pm2 save"
+      pm2.dump(function(err, ret) {
+        bailOnErr(err);
+
+        // exit with success!
+        process.exit();
+      });
     });
   });
 
@@ -76,3 +84,38 @@ function bailOnErr(err) {
     process.exit(1);
   }
 }
+
+
+function updateNOFileConfig(cb) {
+  // const numCPU = os.cpus().length;
+  const totalmemMB = os.totalmem() / 1024 / 1024;
+  const estimatedCCUPerGB = 4000;
+
+  const maxCCU = (totalmemMB / 1024) * estimatedCCUPerGB;
+  const systemMaxNOFileLimit = maxCCU * 4;
+  const nginxMaxNOFileLimit = maxCCU * 3; // 3x because of nginx -> proxy_pass -> node:port
+
+  // immediatelly apply new nofile limit
+  exec(`ulimit -n ${systemMaxNOFileLimit}`, bailOnErr);
+
+  // update "/etc/security/limits.conf" file.
+  fs.writeFileSync("/etc/security/limits.conf", `
+* - nofile $NOFILE_LIMIT
+`, bailOnErr);
+
+  if (fs.existsSync(NGINX_LIMITS_CONFIG_FILE)) {
+    fs.writeFileSync(NGINX_LIMITS_CONFIG_FILE, `
+worker_rlimit_nofile ${nginxMaxNOFileLimit};
+
+events {
+    worker_connections ${maxCCU};
+    # multi_accept on;
+}
+`, cb);
+    console.log("new nofile limit:", { maxCCU, systemMaxNOFileLimit, nginxMaxNOFileLimit });
+
+  } else {
+    console.warn(NGINX_LIMITS_CONFIG_FILE, "not found.");
+  }
+}
+
