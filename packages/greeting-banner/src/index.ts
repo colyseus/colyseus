a4d27258c4385eeeef73012537f4ecc27435b271
Endel Dreyer <endel.dreyer@gmail.com>
2023-05-12 11:00:51 -0300

    add colyseus-cloud banner

--- packages/greeting-banner/src/index.ts
+++ packages/greeting-banner/src/index.ts
@@ -1,4 +1,17 @@
-export default String.raw`
+export default ((process.env.COLYSEUS_CLOUD)
+  ? String.raw`
+   ______      __                              ________                __
+  / ____/___  / /_  __________  __  _______   / ____/ /___  __  ______/ /
+ / /   / __ \/ / / / / ___/ _ \/ / / / ___/  / /   / / __ \/ / / / __  /
+/ /___/ /_/ / / /_/ (__  )  __/ /_/ (__  )  / /___/ / /_/ / /_/ / /_/ /
+\____/\____/_/\__, /____/\___/\__,_/____/   \____/_/\____/\__,_/\__,_/
+             /____/
+
+🚀 Thank you for using Colyseus Cloud
+❓ If you need help please reach out on support@colyseus.io
+`
+
+  : String.raw`
        ___      _
       / __\___ | |_   _ ___  ___ _   _ ___
      / /  / _ \| | | | / __|/ _ \ | | / __|
@@ -11,4 +24,4 @@ Multiplayer Framework for Node.js · Open-source
 💖 Sponsor Colyseus on GitHub → https://github.com/sponsors/endel
 🌟 Give it a star on GitHub → https://github.com/colyseus/colyseus
 ☁️  Deploy and scale your project on Colyseus Cloud → https://cloud.colyseus.io
-`;
+`);
