9abff5fbe076ac5c7d084fe425ff5a165ee0dea9
Endel Dreyer <endel.dreyer@gmail.com>
2023-06-12 09:53:20 -0300

    load .env.cloud before user-defined .env file

--- packages/tools/src/index.ts
+++ packages/tools/src/index.ts
@@ -14,24 +14,44 @@ try {
   uWebSocketsExpressCompatibility = require('uwebsockets-express').default;
 } catch (e) {}
 
-const NODE_ENV = process.env.NODE_ENV || "development";
-const REGION = (process.env.REGION || "unknown").toLowerCase(); // EU, NA, AS, AF, AU, SA, UNKNOWN
-
-const envPaths = [];
-[`.env.${REGION}.${NODE_ENV}`, `.env.${NODE_ENV}`, `.env`].forEach((envFilename) => {
-  envPaths.push(path.resolve(path.dirname(require?.main?.filename || process.cwd()), "..", envFilename));
-  envPaths.push(path.resolve(process.cwd(), envFilename));
-});
-
-// return the first .env path found
-const envPath = envPaths.find((envPath) => fs.existsSync(envPath));
-
-if (envPath) {
-    dotenv.config({ path: envPath });
-    logger.info(`✅ ${path.basename(envPath)} loaded.`);
-} else {
-    logger.info(`⚠️  env file not found.`);
+function getNodeEnv() {
+  return process.env.NODE_ENV || "development";
 }
+
+function getRegion() {
+  // EU, NA, AS, AF, AU, SA, UNKNOWN
+  return (process.env.REGION || "unknown").toLowerCase();;
+}
+
+function loadEnvFile(envFileOptions: string[], log: boolean = false) {
+    const envPaths = [];
+    envFileOptions.forEach((envFilename) => {
+      envPaths.push(path.resolve(path.dirname(require?.main?.filename || process.cwd()), "..", envFilename));
+      envPaths.push(path.resolve(process.cwd(), envFilename));
+    });
+
+    // return the first .env path found
+    const envPath = envPaths.find((envPath) => fs.existsSync(envPath));
+
+    if (envPath) {
+        dotenv.config({ path: envPath });
+
+        if (log) {
+            logger.info(`✅ ${path.basename(envPath)} loaded.`);
+        }
+
+    } else if (log) {
+        logger.info(`⚠️  env file not found.`);
+    }
+}
+
+// load .env.cloud defined on admin panel
+if (process.env.COLYSEUS_CLOUD !== undefined) {
+    loadEnvFile([`.env.cloud`]);
+}
+
+// (overrides previous env configs)
+loadEnvFile([`.env.${getRegion()}.${getNodeEnv()}`, `.env.${getNodeEnv()}`, `.env`], true);
 
 export interface ConfigOptions {
     options?: ServerOptions,
@@ -58,7 +78,7 @@ export default function (options: ConfigOptions) {
     if (!ALLOWED_KEYS[option]) {
       throw new Error(`❌ Invalid option '${option}'. Allowed options are: ${Object.keys(ALLOWED_KEYS).join(", ")}`);
     }
-    if(typeof(options[option]) !== ALLOWED_KEYS[option]) {
+    if(options[option] !== undefined && typeof(options[option]) !== ALLOWED_KEYS[option]) {
       throw new Error(`❌ Invalid type for ${option}: please provide a ${ALLOWED_KEYS[option]} value.`);
     }
   }
