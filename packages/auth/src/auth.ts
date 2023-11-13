733cfe1675013243d5f99bc6903602d4e1d12c20
Endel Dreyer <endel.dreyer@gmail.com>
2023-11-13 18:32:12 -0300

    wip new auth APIs

--- packages/auth/src/auth.ts
+++ packages/auth/src/auth.ts
@@ -2,20 +2,47 @@ import express from 'express';
 import { generateId } from "@colyseus/core";
 import { OAuthCallback, oAuthCallback, oauth } from "./oauth";
 
+export type OnRegisterCallback = (email: string, password: string) => Promise<any>;
+export type OnLoginCallback = (email: string, password: string) => Promise<any>;
+
 export interface AuthSettings {
+  onRegister: OnRegisterCallback,
+  onLogin: OnLoginCallback,
   oAuthCallback: OAuthCallback,
-}
+};
+
+let onLoginCallback: OnLoginCallback = (email: string, password: string) => {
+  return Promise.resolve({});
+};
+
+let onRegisterCallback: OnRegisterCallback = (email: string, password: string) => {
+  return Promise.resolve({});
+};
 
 export const auth = {
   prefix: "/auth",
-  routes: function (settings: Partial<AuthSettings>) {
+
+  routes: function (settings: Partial<AuthSettings> = {}) {
     const router = express.Router();
 
     router.post("/login", (req, res) => {
-      res.json({});
+      res.json({  });
     });
 
-    router.post("/register", (req, res) => {
+    router.post("/register", async (req, res) => {
+      const email = req.body.email;
+      const password = req.body.password;
+
+      if (!/^[^\s@]+@[^\s@]+\.[a-z]{2,}$/i.test(email)) {
+        return res.status(400).json({ error: "Please provide a valid email address." });
+      }
+
+      if (password.length < 5) {
+        return res.status(400).json({ error: "Password too short" });
+      }
+
+      const user = await onRegisterCallback(email, password);
+
       res.json({});
     });
 
--- packages/auth/test/Auth.test.ts
+++ packages/auth/test/Auth.test.ts
@@ -1,11 +1,18 @@
 import assert from "assert";
-import { JsonWebToken, JwtPayload } from "../src/index";
+import { JsonWebToken, JwtPayload, auth } from "../src/index";
+import express from "express";
 
 // JWT Secret for testing
 // JsonWebToken.options.verify.algorithms = ['HS512'];
 JsonWebToken.options.secret = "@%^&";
 
 describe("Auth", () => {
+
+  it("should return express routes", async () => {
+    const app = express();
+    app.use(auth.prefix, auth.routes({ }));
+
+  });
 
   it("should sign and verify token", async () => {
     const data = { id: 1, name: "Jake Badlands", email: "jake@badlands.io" }
