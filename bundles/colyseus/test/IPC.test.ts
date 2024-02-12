bf453e5b98d58a1ea61d5359b68693ddf5237526
Endel Dreyer <endel.dreyer@gmail.com>
2024-02-12 13:50:52 -0300

    preserve error code on IPC errors. fixes #697

--- bundles/colyseus/test/IPC.test.ts
+++ bundles/colyseus/test/IPC.test.ts
@@ -2,6 +2,7 @@ import assert from "assert";
 
 import { PRESENCE_IMPLEMENTATIONS } from "./utils";
 import { subscribeIPC, requestFromIPC } from "@colyseus/core/build/IPC";
+import { ServerError } from "@colyseus/core";
 
 describe("Inter-process Communication", () => {
   for (let i = 0; i < PRESENCE_IMPLEMENTATIONS.length; i++) {
@@ -58,10 +59,46 @@ describe("Inter-process Communication", () => {
 
         try {
           await requestFromIPC(presence, channel, "anything", []);
-          throw new Error("should have errored.");
+          assert.fail("should have errored.");
+
+        } catch (e) {
+          assert.ok(e instanceof Error);
+          assert.strictEqual(e.message, "error message");
+        }
+      });
+
+      it("should reject with plain error string", async () => {
+        const channel = 'error-channel';
+
+        subscribeIPC(presence, "anything", channel, (method, args) => {
+          throw "error message";
+        });
+
+        try {
+          await requestFromIPC(presence, channel, "anything", []);
+          assert.fail("should have errored.");
+
+        } catch (e) {
+          assert.ok(e instanceof Error);
+          assert.strictEqual(e.message, "error message");
+        }
+      });
+
+      it("ServerError: should reject containing error code", async () => {
+        const channel = 'error-code';
+
+        subscribeIPC(presence, "anything", channel, (method, args) => {
+          throw new ServerError(1000, "error message");
+        });
+
+        try {
+          await requestFromIPC(presence, channel, "anything", []);
+          assert.fail("should have errored.");
 
         } catch (e) {
           assert.strictEqual(e.message, "error message");
+          assert.strictEqual(e.code, 1000);
+          // assert.ok(e instanceof Error);
         }
       });
 
