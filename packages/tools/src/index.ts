e83c8dd3e8704871dc2b4ea8a6df48c810df4fd8
Endel Dreyer <endel.dreyer@gmail.com>
2024-04-30 19:30:18 -0300

    @colyseus/tools: fixes loading env vars before importing @colyseus/core

--- packages/tools/src/index.ts
+++ packages/tools/src/index.ts
@@ -1,55 +1,9 @@
-import fs from "fs";
+import "./loadenv";
 import os from "os";
 import http from "http";
-import path from "path";
 import cors from "cors";
 import express from "express";
-import dotenv from "dotenv";
 import osUtils from "node-os-utils";
-
-function getNodeEnv() {
-  return process.env.NODE_ENV || "development";
-}
-
-function getRegion() {
-  // EU, NA, AS, AF, AU, SA, UNKNOWN
-  return (process.env.REGION || "unknown").toLowerCase();
-}
-
-function loadEnvFile(envFileOptions: string[], log: 'none' | 'success' | 'both'  = 'none') {
-    const envPaths = [];
-    envFileOptions.forEach((envFilename) => {
-      envPaths.push(path.resolve(path.dirname(require?.main?.filename || process.cwd()), "..", envFilename));
-      envPaths.push(path.resolve(process.cwd(), envFilename));
-    });
-
-    // return the first .env path found
-    const envPath = envPaths.find((envPath) => fs.existsSync(envPath));
-
-    if (envPath) {
-        dotenv.config({ path: envPath });
-
-        if (log !== "none") {
-            console.info(`✅ ${path.basename(envPath)} loaded.`);
-        }
-
-    } else if (log === "both") {
-        console.info(`ℹ️  optional .env file not found: ${envFileOptions.join(", ")}`);
-    }
-}
-
-// load .env.cloud defined on admin panel
-if (process.env.COLYSEUS_CLOUD !== undefined) {
-    loadEnvFile([`.env.cloud`]);
-}
-
-// (overrides previous env configs)
-loadEnvFile([`.env.${getNodeEnv()}`, `.env`], 'both');
-
-if (process.env.REGION !== undefined) {
-  loadEnvFile([`.env.${getRegion()}.${getNodeEnv()}`], 'success');
-}
-
 import { logger, Server, ServerOptions, Transport, matchMaker } from '@colyseus/core';
 import { WebSocketTransport } from '@colyseus/ws-transport';
 
---
+++ packages/tools/src/loadenv.ts
@@ -0,0 +1,46 @@
+import fs from "fs";
+import path from "path";
+import dotenv from "dotenv";
+
+function getNodeEnv() {
+  return process.env.NODE_ENV || "development";
+}
+
+function getRegion() {
+  // EU, NA, AS, AF, AU, SA, UNKNOWN
+  return (process.env.REGION || "unknown").toLowerCase();
+}
+
+function loadEnvFile(envFileOptions: string[], log: 'none' | 'success' | 'both'  = 'none') {
+    const envPaths = [];
+    envFileOptions.forEach((envFilename) => {
+      envPaths.push(path.resolve(path.dirname(require?.main?.filename || process.cwd()), "..", envFilename));
+      envPaths.push(path.resolve(process.cwd(), envFilename));
+    });
+
+    // return the first .env path found
+    const envPath = envPaths.find((envPath) => fs.existsSync(envPath));
+
+    if (envPath) {
+        dotenv.config({ path: envPath });
+
+        if (log !== "none") {
+            console.info(`✅ ${path.basename(envPath)} loaded.`);
+        }
+
+    } else if (log === "both") {
+        console.info(`ℹ️  optional .env file not found: ${envFileOptions.join(", ")}`);
+    }
+}
+
+// load .env.cloud defined on admin panel
+if (process.env.COLYSEUS_CLOUD !== undefined) {
+    loadEnvFile([`.env.cloud`]);
+}
+
+// (overrides previous env configs)
+loadEnvFile([`.env.${getNodeEnv()}`, `.env`], 'both');
+
+if (process.env.REGION !== undefined) {
+  loadEnvFile([`.env.${getRegion()}.${getNodeEnv()}`], 'success');
+}
