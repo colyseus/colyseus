37f2686fd69f23dae199826b5bcccc0d875adb9a
Endel Dreyer <endel.dreyer@gmail.com>
2025-10-30 16:01:53 -0300

    debug: only debugError logs into stderr. default to stdout.

--- packages/core/src/Debug.ts
+++ packages/core/src/Debug.ts
@@ -3,12 +3,26 @@ import { logger } from './Logger.ts';
 import { ServerError } from './errors/ServerError.ts';
 
 export const debugConnection = debug('colyseus:connection');
+debugConnection.log = console.debug.bind(console); // STDOUT
+
 export const debugDriver = debug('colyseus:driver');
-export const debugError = debug('colyseus:errors');
+debugDriver.log = console.debug.bind(console); // STDOUT
+
 export const debugMatchMaking = debug('colyseus:matchmaking');
+debugMatchMaking.log = console.debug.bind(console); // STDOUT
+
 export const debugMessage = debug('colyseus:message');
+debugMessage.log = console.debug.bind(console); // STDOUT
+
 export const debugPatch = debug('colyseus:patch');
+debugPatch.log = console.debug.bind(console); // STDOUT
+
 export const debugPresence = debug('colyseus:presence');
+debugPresence.log = console.debug.bind(console); // STDOUT
+
+export const debugError = debug('colyseus:errors');
+debugError.log = console.error.bind(console); // STDERR
+
 
 export const debugAndPrintError = (e: Error | string) => {
   const message = (e instanceof Error) ? e.stack : e;
