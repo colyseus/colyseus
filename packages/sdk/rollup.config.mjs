7bc6c27b42bd6a93d77cbf9f20d8619bd9d05a4f
Endel Dreyer <endel.dreyer@gmail.com>
2026-01-09 14:08:18 -0300

    sdk: fix CJS build

--- packages/sdk/rollup.config.mjs
+++ packages/sdk/rollup.config.mjs
@@ -34,7 +34,7 @@ export default [
 
     {
         input: ['src/index.ts'],
-        output: [{ preserveModules: true, banner, dir: 'build/cjs', format: 'cjs', entryFileNames: '[name].js', sourcemap: true },],
+        output: [{ preserveModules: true, banner, dir: 'build/cjs', format: 'cjs', entryFileNames: '[name].cjs', sourcemap: true },],
         external,
         plugins: [
             replacePlugin,
